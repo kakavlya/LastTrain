df0700022f883670ab72bcdbe8ca1fb6dbfa4c8d
casterHoster <casterhoster@gmail.com>
2025-09-06 18:15:39 +0300

    generator is done

--- Assets/Source/Scripts/Level/ObjectsRandomizer.cs
+++ Assets/Source/Scripts/Level/ObjectsRandomizer.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using SplineMesh;
+
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -6,34 +8,99 @@ using UnityEditor;
 
 public class ObjectsRandomizer : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _spawnObjects;
-    [SerializeField] private int _spawnCount;
+    [SerializeField] private MeshRenderer _planeMeshRenderer;
     [SerializeField] private Transform _transformParent;
 
     [Header("Objects Settings")]
+    [SerializeField] private GameObject[] _nearObjects;
+    [SerializeField] private GameObject[] _farObjects;
+    [SerializeField] private int _spawnCount;
     [SerializeField] private float _maxScale;
+
+    [Header("Spline Settings")]
+    [SerializeField] private Spline _spline;
+    [SerializeField] private float _roadOffsetNear;
+    [SerializeField] private float _roadOffsetDistant;
 
     private Vector3 _planeSize;
 
-    private void SpawnObjects()
+#if UNITY_EDITOR
+    public void SpawnObjects(GameObject[] spawnObjects, float minDist, float maxDist)
     {
-        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
-
-        _planeSize = meshRenderer.bounds.size;
+        _planeSize = _planeMeshRenderer.bounds.size;
 
         for (int i = 0; i < _spawnCount; i++)
         {
-            GameObject prefab = _spawnObjects[Random.Range(0, _spawnObjects.Length)];
+            GameObject prefab = spawnObjects[Random.Range(0, spawnObjects.Length)];
 
             float randomX = Random.Range(-_planeSize.x / 2, _planeSize.x / 2);
             float randomZ = Random.Range(-_planeSize.z / 2, _planeSize.z / 2);
 
-            Vector3 spawnPos = transform.position + new Vector3(randomX, 0, randomZ);
+            Vector3 spawnPos = _planeMeshRenderer.transform.position + new Vector3(randomX, 0, randomZ);
+
+            var projection = _spline.GetProjectionSample(spawnPos);
+            float distToRoad = Vector3.Distance(spawnPos, projection.location);
+
+            if (distToRoad < minDist || distToRoad > maxDist)
+                continue;
 
             GameObject instance = Instantiate(prefab, _transformParent);
             instance.transform.position = spawnPos;
             instance.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
-            instance.transform.localScale *= Random.Range(0.8f, _maxScale);
+            float minScale = instance.transform.localScale.x;
+            float scale = Random.Range(minScale, _maxScale);
+            Vector3 baseScale = instance.transform.localScale;
+            instance.transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+#endif
+
+    public void SpawnNearObjects()
+    {
+        SpawnObjects(_nearObjects, _roadOffsetNear, _roadOffsetDistant);
+    }
+
+    public void SpawnFarObjects()
+    {
+        SpawnObjects(_farObjects, _roadOffsetDistant, float.MaxValue);
+    }
+
+#if UNITY_EDITOR
+    public void DeleteObjects()
+    {
+        for (int i = _transformParent.childCount - 1; i >= 0; i--)
+        {
+            DestroyImmediate(_transformParent.GetChild(i).gameObject);
+        }
+}
+#endif
+}
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ObjectsRandomizer))]
+public class RandomSpawnerEditorInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ObjectsRandomizer script = (ObjectsRandomizer)target;
+
+        if (GUILayout.Button("—генерировать ближние"))
+        {
+            script.SpawnNearObjects();
+        }
+
+        if (GUILayout.Button("—генерировать дальние"))
+        {
+            script.SpawnFarObjects();
+        }
+
+        if (GUILayout.Button("”далить все"))
+        {
+            script.DeleteObjects();
         }
     }
 }
+#endif
