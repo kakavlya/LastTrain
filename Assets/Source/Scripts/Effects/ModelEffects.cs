3bb45d7265ba3c85c0b4aa81bce3212e8ac091f6
Pavel Ol <megapiharr2008@gmail.com>
2025-09-09 11:20:26 +0300

    removed flash effects on hit

--- Assets/Source/Scripts/Effects/ModelEffects.cs
+++ Assets/Source/Scripts/Effects/ModelEffects.cs
@@ -14,27 +14,11 @@ namespace LastTrain.Effects
         [SerializeField] private AudioClip _hitSound;
         [SerializeField] private AudioClip _deathSound;
 
-        [Header("Flash Effect")]
-        [SerializeField] private Renderer[] _renderersToFlash;
-        [SerializeField] private Color _flashColor = Color.white;
-        [SerializeField] private float _flashDuration = 0.1f;
-
         private Color[] _originalColors;
         private Material[] _materials;
 
         private void Awake()
         {
-            if (_renderersToFlash == null || _renderersToFlash.Length == 0)
-                _renderersToFlash = GetComponentsInChildren<Renderer>();
-
-            _materials = new Material[_renderersToFlash.Length];
-            _originalColors = new Color[_renderersToFlash.Length];
-
-            for (int i = 0; i < _renderersToFlash.Length; i++)
-            {
-                _materials[i] = _renderersToFlash[i].material;
-                _originalColors[i] = _materials[i].color;
-            }
         }
 
         public void PlayHitFX()
@@ -46,7 +30,6 @@ namespace LastTrain.Effects
                 AudioSource.PlayClipAtPoint(_hitSound, transform.position);
 
             StopAllCoroutines();
-            StartCoroutine(FlashEffect());
         }
 
         public void PlayDeathFX()
@@ -61,17 +44,6 @@ namespace LastTrain.Effects
 
             if (_deathSound)
                 AudioSource.PlayClipAtPoint(_deathSound, transform.position);
-        }
-
-        private IEnumerator FlashEffect()
-        {
-            for (int i = 0; i < _materials.Length; i++)
-                _materials[i].color = _flashColor;
-
-            yield return new WaitForSeconds(_flashDuration);
-
-            for (int i = 0; i < _materials.Length; i++)
-                _materials[i].color = _originalColors[i];
         }
 
         private GameObject GetRandomVFX(GameObject[] options)
